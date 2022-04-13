d96ca29b05dc22690ebea94ad30cc7814de7aed2
nehabattula08 <battula.n@northeastern.edu>
2022-04-12 20:30:51 -0400

    lambda service

---
+++ index.js
@@ -0,0 +1,14 @@
+// import AWS from 'aws-sdk';
+
+// var emailFromLambda = new AWS.SES({re});
+
+console.log('In Lambda function');
+export function handler(event,context,callback){
+  console.log('Message from SNS');
+  var message = event.Records[0].Sns.Message;
+  callback(null,"Success");
+}
+
+//zip the File
+//upload to s3
+//code deploy to aws 
